23d9b4b2c202221ca3a3af0deb16dc24e2e08e8f
g8less <g8less@nym.hush.com>
2025-04-14 19:29:30 -0400

    removing files

--- terraform/aws_ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.ec2_instance.public_ip
-}
-
-output "ssh_command" {
-  description = "SSH command to connect to the EC2 instance"
-  value       = "ssh -i my-key.pem ec2-user@${aws_instance.ec2_instance.public_ip}"
-}
